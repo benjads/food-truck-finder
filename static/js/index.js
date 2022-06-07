4fe6dd348c84060f61debb4798c20ddd37fb237e
cachtayl <54186417+cachtayl@users.noreply.github.com>
2022-06-07 11:05:46 -0700

    See All Button works
    Links to view-review vue function

--- static/js/index.js
+++ static/js/index.js
@@ -13,7 +13,7 @@ let init = (app) => {
         query: "",
         q_truck_results: [],
         q_cuisine_results: [],
-        expanded: -1,
+        reviews_url: "/food-truck-finder/view-reviews/1",
         
         // Upload Images
         selection_done: false,
@@ -28,7 +28,13 @@ let init = (app) => {
         review_add_mode: false,
         current_user: null,
     };
-
+    app.view_reviews_url = (idx) => {
+        let truck = app.vue.trucks[idx];
+        app.vue.reviews_url = "/food-truck-finder/view-reviews/" + truck.id;
+        
+        console.log(app.vue.reviews_url)
+        return app.vue.reviews_url
+    }
     app.enumerate = (a) => {
         // This adds an _idx field to each element of the array.
         let k = 0;
@@ -190,7 +196,7 @@ let init = (app) => {
         // map.setZoom(16);
         // map.panTo(truck.marker.position);
     };
-
+    
     // This is for the sidebar to expand the map when clicked on
     app.toggle_expand_truck = (idx) => {
         let truck = app.vue.trucks[idx];
@@ -216,6 +222,7 @@ let init = (app) => {
     // This contains all the methods.
     app.methods = {
         add_review: app.add_review,
+        view_reviews_url: app.view_reviews_url,
         delete_review: app.delete_review,
         select_file: app.select_file,
         set_stars: app.set_stars,
